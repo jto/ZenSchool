598d2e03c80fe8f75a83a6f3673bf5745ba7b1f1
jtournay <jtournay@linkedin.com>
2012-09-10 07:53:39 -0700

    Make all tests compile but fail

--- test/ClassesSpec.scala
+++ test/ClassesSpec.scala
@@ -3,60 +3,79 @@ package test
 import org.specs2.mutable._
 
 import provided._
+import provided.Helpers._
 import exercices.Classes._
 
 class ClassesSpec extends Specification {
 
-  val prof = new Man(firstname = "Hubert", lastname = "Fansworth", age = 160)
-  val leela = new Woman(firstname = "Leela", lastname = "Turanga", age = 27)
-  val scruffy = new Gentleman(firstname = "Scruffy", lastname = "Thejanitor", age = 42)
-  val mom = new Lady(firstname = "??", lastname = "??", age = 170)
-  val crazyold = new Gentleman(firstname = "Crazy old cat woman", lastname = "??", age = 140)
+  def prof: Man = TODO //new Man(firstname = "Hubert", lastname = "Fansworth", age = 160)
+  def leela: Woman = TODO //new Woman(firstname = "Leela", lastname = "Turanga", age = 27)
+  def scruffy: Gentleman = TODO //new Gentleman(firstname = "Scruffy", lastname = "Thejanitor", age = 42)
+  def mom: Lady = TODO //new Lady(firstname = "??", lastname = "??", age = 170)
+  def crazyold: Granny = TODO //new Granny(firstname = "Crazy old cat woman", lastname = "??", age = 140)
 
   "A Man" should {
     "be a Male" in {
-      prof.sex shouldEqual(Sex.Male)
+      TODO
+      success
+      // prof.sex shouldEqual(Sex.Male)
     }
   }
 
   "A Woman" should {
     "be a Female" in {
-      leela.sex shouldEqual(Sex.Female)
+      TODO
+      success
+      // leela.sex shouldEqual(Sex.Female)
     }
   }
 
   "A Gentleman" should {
     "be a Male" in {
-      scruffy.sex shouldEqual(Sex.Male)
+      TODO
+      success
+      // scruffy.sex shouldEqual(Sex.Male)
     }
 
     "know how to greet" in {
-      scruffy.greet(leela) shouldNot beEmpty
+      TODO
+      success
+      // scruffy.greet(leela) shouldNot beEmpty
     }
   }
 
   "A Lady" should {
     "be a Female" in {
-      mom.sex shouldEqual(Sex.Female)
+      TODO
+      success
+      // mom.sex shouldEqual(Sex.Female)
     }
 
     "know how to greet" in {
-      mom.greet(leela) shouldNot beEmpty
+      TODO
+      success
+      // mom.greet(leela) shouldNot beEmpty
     }
   }
 
   "A Granny" should {
     "be a Female" in {
-      crazyold.sex shouldEqual(Sex.Female)
+      TODO
+      success
+      // crazyold.sex shouldEqual(Sex.Female)
     }
 
     "know how to greet" in {
-      crazyold.greet(leela) shouldNot beEmpty
+      TODO
+      success
+      // crazyold.greet(leela) shouldNot beEmpty
     }
 
     "know how to knit" in {
-      crazyold.knit(leela).color shouldEqual Color.Red
-      crazyold.knit(prof).color shouldEqual Color.Blue
+      TODO
+      success
+      // crazyold.knit(leela).color shouldEqual Color.Red
+      // crazyold.knit(prof).color shouldEqual Color.Blue
     }
   }
 }
--- test/CompanionsSpec.scala
+++ test/CompanionsSpec.scala
@@ -3,23 +3,28 @@ package test
 import org.specs2.mutable._
 
 import provided._
+import provided.Helpers._
 import exercices.Companions._
 
 class CompanionsSpec extends Specification {
 
   "a User" should {
     "have a factory method" in {
-      val u = User("jto", "s3cr3t")
-      u.login shouldEqual("jto")
-      u.password shouldEqual("s3cr3t")
+      TODO
+      success
+      // val u = User("jto", "s3cr3t")
+      // u.login shouldEqual("jto")
+      // u.password shouldEqual("s3cr3t")
     }
 
     "be usable in pattern matching" in {
-      val u = User("jto", "s3cr3t")
-      u match {
-        case User("jto", "s3cr3t") => success
-        case _ => fail("Pattern matching on User failed")
-      }
+      TODO
+      success
+      // val u = User("jto", "s3cr3t")
+      // u match {
+      //   case User("jto", "s3cr3t") => success
+      //   case _ => fail("Pattern matching on User failed")
+      // }
     }
   }
 }
