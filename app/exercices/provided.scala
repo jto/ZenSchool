05c7f9833ce929f5aed68defb2a7c586035a9173
jtournay <jtournay@linkedin.com>
2012-09-11 07:51:21 -0700

    provide stuff

--- app/exercices/provided.scala
+++ app/exercices/provided.scala
@@ -14,4 +14,23 @@ class TODOException extends RuntimeException("TODO")
 
 object Helpers {
   def TODO = throw new TODOException
+
+  import scala.util.control.Exception._
+  /**
+   * This method takes a String, try to parse it as an It
+   * if it fails to parse the String, it returns an empty list
+   * otherwise, it returns a List of one Int
+   */
+  def parseInt(s: String): Either[Throwable, Int] = catching(classOf[NumberFormatException]).either{
+    Integer.parseInt(s)
+  }
+}
+
+package models {
+  case class User(id: Int, login: String)
+  object DAO {
+    private val data: Map[Int, User] = (for(i <- 1.to(20)) yield (i -> User(i, "User_" + i))).toMap
+
+    def get(id: Int): List[User] = data.get(id).toList
+  }
 }
